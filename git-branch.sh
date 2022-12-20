1f180e40f1efbd21ec9f45889a2729594da98f3a
Srgr0 <66754887+Srgr0@users.noreply.github.com>
2022-12-20 09:35:36 +0900

    fix: branches.txtをcurrent directoryに保存

--- git-branch.sh
+++ git-branch.sh
@@ -1,1 +1,1 @@
-git branch -r | tr -s ' ' | cut -d ' ' -f 2 | sed -e 's/*//g' -e 's/origin\///g' | tr '\n' ',' |  sed -e 's/,$//g' > /content/MMVC_Trainer/branches.txt
+git branch -r | tr -s ' ' | cut -d ' ' -f 2 | sed -e 's/*//g' -e 's/origin\///g' | tr '\n' ',' |  sed -e 's/,$//g' > ./branches.txt
